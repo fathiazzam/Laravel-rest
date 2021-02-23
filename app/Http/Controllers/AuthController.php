e3e947073769ee18e18a88876580e5571b77cad1
Fathi Nur Azzam <fathiazzam14@gmail.com>
2021-02-23 15:15:14 +0700

    Learning Rest API
    Using LARAVEL to Restapi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Meeting;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:5',
+        ]);
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password)
+        ]);
+
+        if ($user->save()) {
+            $user->signin = [
+                'href' => 'api/v1/user/signin',
+                'method' => 'POST',
+                'params' => 'email,password'
+            ];
+            $response = [
+                'msg' => 'User Created',
+                'user' => $user
+            ];
+            return response()->json($response, 201);
+        }
+
+        $response = [
+            'msg' => 'Telah terjadi error'
+        ];
+        return response()->json($respons, 404);
+
+    }
+    public function signin(Request $request)
+    {
+        return 'bekerja dengan baik';
+    }
+}
